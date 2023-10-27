005ea0961dfdf194e02a77f260f628b1fdf52a43
Nastya <nastyazabol80@gmail.com>
2023-10-27 14:25:06 +0300

    footerdone

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.scss'
 import Header from '@/components/layout/header/Header'
+import Footer from '@/components/layout/footer/Footer'
 
 const montserrat = Montserrat({
 	subsets: ['latin'],
@@ -23,6 +24,7 @@ export default function RootLayout({
 			<body className={montserrat.className}>
 				<Header />
 				{children}
+				<Footer/>
 			</body>
 		</html>
 	)
