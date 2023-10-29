d9b929c8ab0838edec659617d1e253223e369dde
Nastya <nastyazabol80@gmail.com>
2023-10-29 23:46:47 +0200

    window

---
+++ client/src/components/layout/header/header-top/modal/Modal.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {GrClose} from 'react-icons/gr'
+import s from './modal.module.scss'
+import { IModalProps } from '@/interfaces/header-top-module.interface';
+
+
+const Modal:React.FC<IModalProps> = ({ isOpen = false, onCloseRequest }) => {
+	if (!isOpen) {
+    return null;
+  }
+
+	return (
+		<div className={s.modal} >
+			<div className={s.modalWrap}>
+				<div className={s.modalItems}>
+					<GrClose onClick={onCloseRequest} className={s.close}/>
+					<div className={s.content}>
+						<input type="text" className={s.search} placeholder='Brands, items or categories'/>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+export default Modal;
---
+++ client/src/interfaces/header-top-module.interface.ts
@@ -0,0 +1,4 @@
+export interface IModalProps {
+  isOpen: boolean;
+  onCloseRequest(data: object): void;
+}
